03a8bae3abe8528d059c3adffabe6317bcdd1e16
JigneshSatam <jigneshsatam@gmail.com>
2017-10-13 19:44:22 +0530

    Added button onclicklistner

--- app/src/main/java/com/idfy/firstapplication/Calculator.java
+++ app/src/main/java/com/idfy/firstapplication/Calculator.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.nio.charset.CharacterCodingException;
 
@@ -16,14 +18,62 @@ public class Calculator extends AppCompatActivity implements View.OnClickListene
         setContentView(R.layout.activity_calculator);
         Intent i = getIntent();
         setTitle(i.getStringExtra("userName"));
+
+        Button one = (Button) findViewById(R.id.one);
+        one.setOnClickListener(this);
+
+        Button two = (Button) findViewById(R.id.two);
+        two.setOnClickListener(this);
+
+        Button three = (Button) findViewById(R.id.three);
+        three.setOnClickListener(this);
+
+        Button four = (Button) findViewById(R.id.four);
+        four.setOnClickListener(this);
+
+        Button five = (Button) findViewById(R.id.five);
+        five.setOnClickListener(this);
+
+        Button six = (Button) findViewById(R.id.six);
+        six.setOnClickListener(this);
+
+        Button seven = (Button) findViewById(R.id.seven);
+        seven.setOnClickListener(this);
+
+        Button eight = (Button) findViewById(R.id.eight);
+        eight.setOnClickListener(this);
+
+        Button nine = (Button) findViewById(R.id.nine);
+        nine.setOnClickListener(this);
+
+        Button zero = (Button) findViewById(R.id.zero);
+        zero.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         Button b = (Button) view;
         String str = String.valueOf(b.getText());
-//        switch (str){
-//            case ""
-//        }
+        TextView v = (TextView) findViewById(R.id.result);
+        String expression = String.valueOf(v.getText());
+        switch (str){
+            case "+":
+                char last = expression.charAt(expression.length());
+                if (last == '-'){
+
+                }
+                break;
+            case "-":
+                break;
+            case "*":
+                break;
+            case "/":
+                break;
+            case "=":
+                break;
+            default:
+                String newExpression = expression + str;
+                v.setText(newExpression);
+        }
     }
 }
